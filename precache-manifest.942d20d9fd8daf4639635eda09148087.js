48101d23c7a32ed80e2d24ae11d59e377d7da36e
Neelam Chaurasia <chaurasia.neelam@gmail.com>
2019-03-27 22:30:49 -0400

    Updates

---
+++ precache-manifest.942d20d9fd8daf4639635eda09148087.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ac3498ac07d38a0780e",
+    "url": "/react-ToDoList/static/js/runtime~main.66c51c29.js"
+  },
+  {
+    "revision": "fd008cb3bbff4fa400a0",
+    "url": "/react-ToDoList/static/js/main.9f35ed44.chunk.js"
+  },
+  {
+    "revision": "1ba78d9d674c4258ce7f",
+    "url": "/react-ToDoList/static/js/2.02279afc.chunk.js"
+  },
+  {
+    "revision": "fd008cb3bbff4fa400a0",
+    "url": "/react-ToDoList/static/css/main.853875e7.chunk.css"
+  },
+  {
+    "revision": "1ba78d9d674c4258ce7f",
+    "url": "/react-ToDoList/static/css/2.94833a9d.chunk.css"
+  },
+  {
+    "revision": "6468690733dffe5475cc97d2b318ef4b",
+    "url": "/react-ToDoList/index.html"
+  }
+];
